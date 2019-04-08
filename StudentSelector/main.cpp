4e6f0ea21581a4438f50cf2850fdc59505445b49
npc <fabrodaqp@gmail.com>
2019-04-07 19:56:03 -0500

    Code

---
+++ StudentSelector/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+
+using namespace std;
+
+void genvector(vector<double> porcent, vector<double>probs) {
+	int tempo = 0;
+	while (tempo < probs.size())
+	{
+		for (int i = 0; i < ; i++)
+		{
+
+		}
+	}
+}
+
+int main()
+{
+	return 0;
+}
